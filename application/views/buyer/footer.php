6a4ebb79c8603b60462e3828b92c938c5e559cb0
unknown <meghasangmesh2@gmail.com>
2022-03-02 16:14:38 +0530

    changes on new aucjunction lite on march

--- application/views/buyer/footer.php
+++ application/views/buyer/footer.php
@@ -1,43 +1,25 @@
+  <nav class="nav nav-pills nav-justified mobile-bottom-nav ">
+                 <a class="nav-item nav-link "  data-toggle="tab" href="#nav-home"><i class="fas fa-home"></i> Home</a>
+                <a class="nav-item nav-link" data-toggle="tab" href="#nav-profile"><i class="fa-solid fa-gavel"></i> Auctions</a>
+                <a class="nav-item nav-link" data-toggle="tab" href="#nav-profile2"><i class="fas fa-shopping-cart"></i> Cart</a>
+                <a class="nav-item nav-link " data-toggle="tab" href="#nav-profile3"><i class="fas fa-heart"></i> WishList</a>
+            </nav>
+           
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
+  <!-- Vendor JS Files -->
+  <script src="<?php echo base_url()."web_files/";?>assets/vendor/purecounter/purecounter.js"></script>
+  <script src="<?php echo base_url()."web_files/";?>assets/vendor/aos/aos.js"></script>
+  <script src="<?php echo base_url()."web_files/";?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo base_url()."web_files/";?>assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="<?php echo base_url()."web_files/";?>assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="<?php echo base_url()."web_files/";?>assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="<?php echo base_url()."web_files/";?>assets/vendor/php-email-form/validate.js"></script>
 
+  <!-- Template Main JS File -->
+  <script src="<?php echo base_url()."web_files/";?>assets/js/main.js"></script>
 
+</body>
 
-
-
- <nav class="navbar navbar-expand-lg navbar-light fixed-bottom d-flex justify-content-around " style="background-color:white; color:grey; font-size:25px;">
-
- 
- <div class="col-1"><i class="fa fa-home" aria-hidden="true"></i></div>
-<a href="<?php echo base_url();?>buyer_auctionlist"><div class="col-1"><i class="fa fa-gavel" aria-hidden="true"></i></div></a>
-   <div class="col-1"><i class="fa fa-gift" aria-hidden="true"></i></div>
-    <div class="col-1"><i class="fa fa-heart" aria-hidden="true"></i> </div>
-<div class="col-1">  <a href="<?php echo base_url();?>buyer_categories"><i class="typcn typcn-th-small menu-icon"></i> </a>   </div>  
-
- 
- 
-</nav>
-
-  <!-- container-scroller -->
-    <!-- base:js -->
-    <script src="<?php echo base_url()."web_files/";?>vendors/js/vendor.bundle.base.js"></script>
-    <!-- endinject -->
-    <!-- Plugin js for this page-->
-    <!-- End plugin js for this page-->
-    <!-- inject:js -->
-    <script src="<?php echo base_url()."web_files/";?>js/off-canvas.js"></script>
-    <script src="<?php echo base_url()."web_files/";?>js/hoverable-collapse.js"></script>
-    <script src="<?php echo base_url()."web_files/";?>js/template.js"></script>
-    <script src="<?php echo base_url()."web_files/";?>js/settings.js"></script>
-    <script src="<?php echo base_url()."web_files/";?>js/todolist.js"></script>
-    <!-- endinject -->
-    <!-- plugin js for this page -->
-    <script src="<?php echo base_url()."web_files/";?>vendors/progressbar.js/progressbar.min.js"></script>
-    <script src="<?php echo base_url()."web_files/";?>vendors/chart.js/Chart.min.js"></script>
-    <!-- End plugin js for this page -->
-    <!-- Custom js for this page-->
-    <script src="<?php echo base_url()."web_files/";?>js/dashboard.js"></script>
-    
-    <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.11.4/datatables.min.js"></script>
-    <!-- End custom js for this page-->
-  </body>
 </html>
