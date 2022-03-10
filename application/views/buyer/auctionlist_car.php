15ac618f0ac7dc493263eb178975b07c69340288
suma <sumashettyks@gmail.com>
2022-03-10 18:21:17 +0530

    update by suma on 3/10/2022

--- application/views/buyer/auctionlist_car.php
+++ application/views/buyer/auctionlist_car.php
@@ -181,7 +181,7 @@
 		
 	
 	
-   <table class="table table-striped" style="colorgrey;font-size15px;">
+   <table class="table table-striped" style="colorgrey;font-size15px;margin-top:-70px;">
 <tbody>
 	<tr>
      
