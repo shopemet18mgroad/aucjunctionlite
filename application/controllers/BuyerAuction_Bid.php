8c3abfaeb803ef61e088c94596e7682ec2b73d9a
nishant <nishant6000@gmail.com>
2022-04-07 04:09:37 +0530

    bidding

--- application/controllers/BuyerAuction_Bid.php
+++ application/controllers/BuyerAuction_Bid.php
@@ -21,22 +21,66 @@ class BuyerAuction_Bid extends CI_Controller {
 	public function __construct() {
         parent:: __construct();
         $this->load->helper(array('url', 'html', 'date', 'form', 'cookie'));
-		$this->load->library(array('session','email'));
-        //$this->load->library('email');
+		$this->load->library(array('session','email', 'Currency'));
 		$this->load->model('Admin_model');
     }
 	
 	public function index()
 	{
-		$auc = $this->uri->segment(3);
-        $bid = $this->uri->segment(4);
-        //=============================Check if higher or equal bid value exists if no update into cbid in add lot and update my bid======
+		if(!$this->session->has_userdata('username')|| $this->session->userdata('auth') != "BUYER"){
+			$datainserr = "Invalid Login Session";
+			echo "Please Login Again";
+			die;
+			}else{
+				$sessi = $this->session->userdata('username');
+				$auc = $this->uri->segment(3);
+        		$bid = $this->uri->segment(4);
+				$aucn = str_ireplace("-","/",$auc);
 
+			}
+        //=============================Check if higher or equal bid value exists if no update into cbid in add lot and update my bid======
+		date_default_timezone_set("Asia/Kolkata"); 
+			$date = date('Y-m-d H:i:s');
+			$dataotpex = array('iauctionid'=>$aucn,'cbid >='=>$bid);
+			if($this->Admin_model->check('addlot', $dataotpex)){
+				echo "High or Same Bid Value Exists";
+				die;
+			}else{
+				//===Update into AddLot Database===================
+				$table = "addlot";
+				$datadchk = array('iauctionid'=>$aucn,'iauction_end >'=>$date);
+				if($this->Admin_model->check($table, $datadchk)){
+					$table = "addlot";
+					$comp = array('iauctionid'=>$aucn);
+					$datafupbid = array('cbid'=>$bid);
+					$table2 = "cart_payment";
+					$comp2 = array('auction_id'=>$aucn, 'user_email'=>$sessi);
+					$datafupbid2 = array('mybid'=>$bid);
+					if($this->Admin_model->update_custom($table, $datafupbid, $comp, $comp)){
+						echo "Bid Placed Failed";
+					}else{
+						$this->Admin_model->update_custom($table2, $datafupbid2, $comp2, $comp2);
+						//insert to biding summary====================
+
+						//============================================
+						echo "Bid Placed Succesfully";
+					}
+				}else{
+					echo "Auction Closed";
+				}
+
+				
+				//=================================================
+
+			}
         //=============================Check if my lower bid value exists in bidding cart if yes update or insert==========================
 
 
         //=================================================================================================================================
-        
     }
-	
+	public function get_currency(){
+		$data = urldecode($this->uri->segment(3));
+		echo $this->currency->getIndianCurrency($data);
+		die;
+	}
 }
