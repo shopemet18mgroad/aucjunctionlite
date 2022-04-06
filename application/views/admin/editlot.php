3f85f9ab726f16f4e9e65cfa62affa7ef99d1cb0
megha <meghasangmesh2@gmail.com>
2022-04-06 18:46:14 +0530

    changes and catrgory

--- application/views/admin/editlot.php
+++ application/views/admin/editlot.php
@@ -5,17 +5,7 @@
   <div class="container py-5 h-100">
     <div class="row m-4 m-5 m-5 d-flex justify-content-center align-items-center h-100">
       <div class="col-12">
-       
-            
-         
-
-
-                  
-				  
-				  
-				  
-				  
-				  
+   
                  <form action= "<?php echo base_url();?>Admin_editlot_basicinfo_update"
 				 method="post" enctype="multipart/form-data"> 
 			
@@ -32,7 +22,6 @@
                         <select class="form-control form-control-lg" id="icategory" name="icategory" value="<?php echo $sqldata[0]->icategory ?>" >
 						<option value="<?php echo $sqldata[0]->icategory ?>"><?php echo $sqldata[0]->icategory ?></option>
 				
-				
 				<option value="Automobile">Automobile</option>
 				<option value="Home Appliance">Home Appliance</option>
 				<option value="Electronic Appliance">Electronic Appliance </option>
@@ -41,6 +30,10 @@
 					<option value="Furniture">Furniture</option>
 				<option value="Toys">Toys</option>
 				<option value="Fashion">Fashion</option>
+				<option value="TV">TV</option>
+				<option value="Washing machine">Washing machine</option>
+				<option value="Fridge">Fridge</option>
+				<option value="Camera">Camera</option>
 				
 				
 				
@@ -57,18 +50,15 @@
 				
 				<option value="<?php echo $sqldata[0]->isubcategory ?>"><?php echo $sqldata[0]->isubcategory ?></option>
 				
-					<option value="Car">Car</option>
-				<option value="Bike">Bike</option>
-				<option value="Laptop">Laptop</option>
+				<option value="Cars">Cars</option>
+				<option value="Bikes">Bikes</option>
+                <option value="Commercial">Commercial</option>
+				<option value="Laptop" >Laptop</option>
 				<option value="Mobile">Mobile</option>
-				<option value="Properties">Properties</option>
-				<option value="TV">TV</option>
-				<option value="Washing machine">Washing machine</option>
-				<option value="Fridge">Fridge</option>
-				<option value="Camera">Camera</option>
-				<option value="Toys">Toys</option>
-				<option value="Furniture">Furniture</option>
-				<option value="Fashion">Fashion</option>
+                <option value="TV">TV</option>
+                <option value="Washing Machine">Washing Machine</option>
+                <option value="Fridge">Fridge</option>
+				Sub Category
 				
 				
 				</select>
@@ -78,43 +68,30 @@
                     <div class="form-group row m-4">
                       <label for="" class="col-sm-3 col-form-label">product Description</label>
                       <div class="col-sm-9">
-                        <textarea row m-4s="4" cols="5" class="form-control form-control-lg" id="iproductdes" name="iproductdes" placeholder="product Description " value= "<?php echo $sqldata[0]->iproductdes?>" ></textarea>
+                        <textarea class="form-control form-control-lg" id="iproductdes" name="iproductdes" placeholder="product Description " value= "<?php echo $sqldata[0]->iproductdes ?>" ></textarea>
                       </div>
                     </div>
-					
-					 <div class="form-group row m-4">
-					 
-					 <label for="" class="col-sm-3 col-form-label">product Name</label>
-                     
-                      <div class="col-sm-9">
-                        <input type="text" class="form-control form-control-lg" id="iproductname" name="iproductname"  value="<?php echo $sqldata[0]->iproductname ?>" >
-                      </div>
-                    </div>
-					
 				
 					 <div class="form-group row m-4">
                      
                       <div class="col-sm-9">
-                        <input type="hidden" class="form-control form-control-lg" id="sl_noadd" name="sl_noadd"  value="<?php echo $sqldata[0]->sl_noadd ?>" >
+                        <input type="hidden" class="form-control form-control-lg" id="sl_ano" name="sl_ano"  value="<?php echo $sqldata[0]->sl_ano ?>" >
                       </div>
                     </div>
+					
+					
+					
 					
 					
 					
                     <div class="form-group row m-4">
-                      <label for="" class="col-sm-3 col-form-label">Inspection Date</label>
+                      <label for="" class="col-sm-3 col-form-label">Inspection Till</label>
                       <div class="col-sm-9">
-                        <input type="datetime-local" class="form-control form-control-lg" id="inspectiondate" name="inspectiondate" placeholder="Inspection Date "  value="<?php echo date('Y-m-d\TH:i', strtotime($sqldata[0]->inspectiondate)); ?>">
+                        <input type="datetime-local" class="form-control form-control-lg" id="inspectiondate" name="inspectiondate" placeholder="Inspection Date " value="<?php echo $sqldata[0]->inspectiondate ?>" >
                       </div>
                     </div>
 					
-					<div class="form-group row m-4">
-                      <label for="" class="col-sm-3 col-form-label">Current location </label>
-                      <div class="col-sm-9">
-                        <input type="text" class="form-control form-control-lg" id="currentlocation" name="currentlocation" placeholder="current location " value="<?php echo $sqldata[0]->currentlocation ?>" >
-                      </div>
-                    </div>
-                   
+				
 					
 					
                    
@@ -126,46 +103,12 @@
                     </div>
                     
                     <div class="form-group row m-4">
-                      <label for="" class="col-sm-3 col-form-label">Start  Auction Price</label>
+                      <label for="" class="col-sm-3 col-form-label">Start Auction Price</label>
                       <div class="col-sm-9">
                         <input type="text" class="form-control form-control-lg" id="startaucprice" name="startaucprice" placeholder="Start Auction Price"value="<?php echo $sqldata[0]->startaucprice ?>" >
                       </div>
                     </div>
-                    
-                       <div class="form-group row m-4">
-                      <label for="" class="col-sm-3 col-form-label">End Auction Price</label>
-                      <div class="col-sm-9">
-                        <input type="text" class="form-control form-control-lg" id="endaucprice" name="endaucprice" placeholder="endaucprice" value="<?php echo $sqldata[0]->endaucprice ?>">
-                      </div>
-                    </div>
-					
-					     <div class="form-group row m-4">
-                      <label for="" class="col-sm-3 col-form-label">entry free </label>
-                      <div class="col-sm-9">
-                        <input type="text" class="form-control form-control-lg" id="entryfee" name="entryfee" placeholder="entryfee" value="<?php echo $sqldata[0]->entryfee ?>" >
-                      </div>
-                    </div>
-					
-                   
-				  
-						 <div class="form-group row m-4">
-                      <label for="" class="col-sm-3 col-form-label">Inspection date and time</label>
-                      <div class="col-sm-9">
-                       From: 	
-                    
-						
-				  <input class="form-control form-control-lg " type="datetime-local"  id="iauction_start" name="iauction_start" value="<?php echo date('Y-m-d\TH:i', strtotime($sqldata[0]->iauction_start)); ?>">		
-						
-						<br>
-                         
-
-To: 
-							
-							
-   <input class="form-control form-control-lg " type="datetime-local"  id="iauction_end" name="iauction_end" value="<?php echo date('Y-m-d\TH:i', strtotime($sqldata[0]->iauction_end)); ?>">
-                      </div>
-                    </div>
-					
+                  
 					
 
 					
@@ -175,17 +118,11 @@ To:
 			 <div class="form-group row m-4">
                       <label for="" class="col-sm-3 col-form-label">Upload photos</label>
                       <div class="col-sm-9">
-                        <input type="file" class="mt-3" id="imageupload" name="imageupload[]" multiple >
+                        <input type="file" class="mt-3" id="imageupload" name="imageupload[]" multiple placeholder="End Auction Price" value="<?php echo $sqldata[0]->imageupload ?>" >
 						  <?php $img = unserialize($sqldata[0]->imageupload)?>
-						  
-						<?php foreach($img as $sql){ ?>
-							
-						<img class="model" src="<?php echo base_url()."web_files/uploads/".$sql;?>" 
- alt="Chania" width="60px" height="60px">	
-							
-					<?php	} ?>  
 						
-						
+						<img class="model" src="<?php echo base_url()."web_files/uploads/".$img[0];?>" 
+ alt="Chania" width="200px" height="250px">
 						
 						
                       </div>
@@ -201,7 +138,6 @@ To:
                  <center><button type="submit " onclick="return validateaddlot()" name="submit" class="btn btn-info mr-2 w-50">Submit</button>
                     <button class="btn btn-light">Cancel</button></center>
                   </form>
-           
       
 		   </div>
 		   </div>
