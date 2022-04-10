176834fde3c66f45c57e1a10201512810d6c1a55
nishant <nishant6000@gmail.com>
2022-04-11 02:47:38 +0530

    sunday Reports

--- application/views/buyer/aucparticipate.php
+++ application/views/buyer/aucparticipate.php
@@ -19,11 +19,11 @@
             
               <div class="col-md-3 col-lg-3 col-xl-3">
                 <p class="lead fw-normal mb-2" style="font-size:18px;">Auction ID: <?php echo $mycom->iauctionid; ?></p>
-                <p><span class="text-muted">Date: <?php echo $mycom->iauction_end; ?></span></p>
+                <p><span class="text-muted">Date: <?php echo str_ireplace(".000000","",$mycom->iauction_end);?></span></p>
               </div>
               
               <div class="col-md-5 col-lg-5 col-xl-5 offset-lg-1">
-			  <h5 class="mb-0"style="font-size:18px;">Max Bid Value : <?php  echo $worldata[3]; ?></h5>
+			  <h5 class="mb-0"style="font-size:18px;">Winning Bid Value : <?php  echo $worldata[3]; ?></h5>
         <h5 class="mb-0"style="font-size:18px;">Auction Winner : <?php  echo $worldata[2]; ?></h5>
                 Result:<span class="mb-0" style="color:green;font-size:18px; "><?php echo $worldata[0];?></span>
               </div>
@@ -58,10 +58,10 @@
   
   
   
-
+<?php if(count($mycomauc)){$i=0;foreach($mycomauc as $mycom){?>
 
 <!-- Modal -->
-<div class="modal fade" id="viewdetail-0" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+<div class="modal fade" id="viewdetail-<?php echo $i;?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
@@ -84,154 +84,78 @@
 
       
       <div class="container mb-5">
-        <div class="row border border-dark">
-        <table class="table table-striped" style="colorgrey;font-size15px;margin-top:-70px;">
-<tbody>
-	<tr>
-     
-      <th scope="row"> Auction Images</th>
-	  
-	   <td data-toggle="modal" data-target="#exampleModal-"><?php ?>
-	   
-	   
-	   
-
-<img class="model" src="" 
- alt="Chania" width="50px" height="50px">
- 
-	   </td>
-      
-    </tr>
-    <tr>   
-   <th scope="row">Category </th>
-   <td><?php  ?></td>
-	  </tr>
-	 <br>
-    <tr>
-     
-      <th scope="row">Sub-category </th>
-	  <td><?php ?></td>
-      
-    </tr><br>
-    <tr>
-     
-      <th scope="row">product Description  </th>
-	  <td style="word-break:break-all;"><?php  ?></td>
-      
-    </tr><br>
-	
-	<tr>
-     
-      <th scope="row">Auction Id </th>
-	    <td><?php ?></td>  
-    </tr>	
-	<tr>
-      <th scope="row"> Auction Start and End Time</th>
-	   <td>
-	  </td>
-	  
-	  
-    </tr>
-	
-	
-	<tr>
-	
-	<td colspan=2><center>  <div class="ab w-100" style="background-color:#2196f363;color:white; font-size:20px;" ><i><!--<?php //echo //$sqldata1[0]->role ?>--></i>LOT  DETAILS  </div></center></td>
-	</tr>
-	
-	<tr>
-     
-      <th scope="row">Inspection Date </th>
-
-	   
-	   <td><?php 	   
-	   ?>
-	     
-	   </td>
-      
-    </tr>
-	
-		
-		<tr>
-          <th scope="row">MRP <i class="fa fa-inr" style="font-size:18px"></i></th>
-	   <td><?php  ?></td>
-      
-    </tr>
-	
-		<tr>
-     
-      <th scope="row"> Start  Auction Price</th>
-	   <td><?php  ?></td>
-      
-    </tr>
-	
-		<tr>
-     
-      <th scope="row">End Auction Price  </th>
-	   <td><?php  ?></td>
-      
-    </tr>
-	
-		<tr>
-     
-      <th scope="row">Entry Fee </th>
-	   <td><?php ?></td>
-      
-    </tr>
-	
-	
-	
-	
-	
-		<tr>
-     
-      <th scope="row">Inspection date and time  </th>
-	     
-	   <td><?php ?>
-	   
-	   <?php ?>
-	  </td>
-	  
-	   </td>
-       
-    </tr>
-	
-	<tbody>
-	</table>
-        </div>
-          
+        <div class="">
+        <table class="table table-striped" style="">
+          <tbody>
+            <tr>
+            <th scope="row"> Auction Images</th>
+	          <td  data-toggle="modal" data-target="#exampleModal"><?php $img = unserialize($mycom->imageupload)?>
+	              <?php foreach($img as $sql) { ?>
+        <img class="model" src="<?php echo base_url()."web_files/uploads/".$sql;?>" 
+        alt="Chania" width="50px" height="50px">
+            <?php } ?> 
+            </td>  
+          </tr>
+            <tr>   
+              <th scope="row">Category </th>
+              <td><?php echo $mycom->icategory;?></td>
+            </tr>
+            <tr>
+              <th scope="row">Sub-category </th>
+              <td><?php  echo $mycom->isubcategory;?></td>
+            </tr>
+            <tr>
+              <th scope="row">product Description  </th>
+              <td style="word-break:break-all;"><?php  echo $mycom->iproductdes;?></td>
+            </tr>
+            <tr>
+                <th scope="row">Auction Id </th>
+                <td><?php  echo $mycom->iauctionid;?></td>  
+              </tr>	
+            <tr>
+              <th scope="row"> Auction Start Time</th>
+            <td>
+              <?php echo $mycom->iauction_start;?>
+            </td>
+          </tr>
+          <tr>
+              <th scope="row"> Auction End Time</th>
+            <td>
+               <?php echo $mycom->iauction_end;?>
+            </td>
+          </tr>
+	        <tr>
+          <td colspan=2><center>  <div class="ab w-100" style="background-color:#2196f363;color:white; font-size:20px;" ><i><!--<?php //echo //$sqldata1[0]->role ?>--></i>LOT  DETAILS  </div></center></td>
+          </tr>
+          <tr>
+            <th scope="row">Inspection Date </th>
+          <td><?php echo $mycom->inspectiondate;?>
+          </td>
+          </tr>
+              <tr>
+                <th scope="row">MRP <i class="fa fa-inr" style="font-size:18px"></i></th>
+              <td><?php echo $mycom->imrp;?></td>
+              </tr>
+            <tr>
+              <th scope="row"> Start  Auction Price</th>
+              <td><?php echo $mycom->startaucprice;?></td>  
+            </tr>
+            <tr>
+              <th scope="row">End Auction Price  </th>
+            <td><?php echo $mycom->endaucprice;?></td> 
+            </tr>
+            <tr>
+              <th scope="row">Entry Fee </th>
+            <td><?php echo $mycom->entryfee;?></td>
+            </tr>
+           
+	      <tbody>
+	    </table>
+       </div>          
       </div>
-        
-  
-
-     
     </div>
   </section>
-
-  </main><!-- End #main -->  
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-
-                
-                
-                
+  </main><!-- End #main -->                       
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -240,10 +164,11 @@
     </div>
   </div>
 </div>
-
+<?php $i++;}}?>
 
 <!-- Modal -->
-<div class="modal fade" id="bidsummary-0" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+<?php if(count($mybid)){$i=0;foreach($mybid as $mybi){?>
+<div class="modal fade" id="bidsummary-<?php echo $i;?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
@@ -253,9 +178,20 @@
         </button>
       </div>
       <div class="modal-body">
-      Auction ID:<br>22/feb/2020/98/78</p>
-                <p><span class="text-muted">Date:23-23-2909<br>Auction Name :Scopio</h5><br>
-                Result:<span class="mb-0" style="color:green;font-size:18px; ">Won</span>
+        <table class="table table-striped" style="">
+          <tbody>
+              <tr>   
+              <th scope="row">My Bid </th>
+               <th scope="row">Date & Time</th>
+              </tr>
+              <?php if(count($mybi)){ foreach ($mybi as $myb){?>
+              <tr>   
+              <td><?php echo $myb->bidvalue;?></td>
+               <td><?php  echo str_ireplace(".000000","",$myb->Date_time);?></td>
+              </tr>
+               <?php }} ?>
+            </tbody>
+	      </table>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -264,30 +200,10 @@
     </div>
   </div>
 </div>
-
+<?php $i++;}}?>
 
 <!-- Modal -->
-<div class="modal fade" id="bidwinner" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Bid Winner</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-     Auction ID:<br>22/feb/2020/98/78</p>
-                <p><span class="text-muted">Date:23-23-2909<br>Auction Name :Scopio</h5><br>
-                Result:<span class="mb-0" style="color:green;font-size:18px; ">Won</span>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+
 
 
 
