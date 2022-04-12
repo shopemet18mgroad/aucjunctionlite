d6b72c71f43c73e2298c3170cf0cfe6846167a5d
suma <sumashettyks@gmail.com>
2022-04-12 18:30:34 +0530

    update by suma on 4/12/2022

--- application/controllers/Admin_startauction_update.php
+++ application/controllers/Admin_startauction_update.php
@@ -59,7 +59,7 @@ class Admin_startauction_update extends CI_Controller {
 			'iauction_start' => $iauction_start,'iauction_end' => $iauction_end); 
 		
 		
-		$data2 = array('iauctionid' => $iauctionid,'iauction_start' => $iauction_start,'iauction_end' => $iauction_end);
+		$data2 = array('iauctionid' => $iauctionid);
 		
 		          $updatech = array('sl_ano' => $sl_ano);
 	 	if($data2){
