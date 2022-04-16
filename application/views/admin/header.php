13066e469a13c17e898b944d7655b98e071d9bb0
suma <sumashettyks@gmail.com>
2022-04-16 10:53:03 +0530

    update by suma on 4/16/2022

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -156,7 +156,7 @@ function goBack() {
              <li><a href="<?php echo base_url();?>Admin_auctioneditlist">Edit Auction</a></li>
 			 <li><a href="<?php echo base_url();?>Admin_auctionapproval">Approve Forthcoming Auction</a></li>
              
-              <li><a href="<?php echo base_url();?>Admin_auctionwinapproval"> Auction Approval</a></li>
+              <li><a href="<?php echo base_url();?>Admin_auctionwinapproval"> Auction Winner Approval</a></li>
             </ul>
             </li>
 			      	
@@ -165,7 +165,8 @@ function goBack() {
 		
 				
             <li><a href="<?php echo base_url();?>Admin_aucparticipate"> Auction participated</a></li>
-            
+			
+            <li><a href="<?php echo base_url();?>Admin_transactionhistory"> All Transaction History</a></li>
 			 
 			 
             </ul>
