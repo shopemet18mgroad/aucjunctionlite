6e9391b6e73691ca7469f370db6bd5fa67ffe071
suma <sumashettyks@gmail.com>
2022-04-18 14:24:58 +0530

    update by suma on 4/18/2022

---
+++ application/views/header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>AUCJUNCTION LITE</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="<?php echo base_url()."web_files/";?>assets/img/favicon.png" rel="icon">
+  <link href="<?php echo base_url()."web_files/";?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="<?php echo base_url()."web_files/";?>assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="<?php echo base_url()."web_files/";?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php echo base_url()."web_files/";?>assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="<?php echo base_url()."web_files/";?>assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="<?php echo base_url()."web_files/";?>assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="<?php echo base_url()."web_files/";?>assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="<?php echo base_url()."web_files/";?>assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="<?php echo base_url()."web_files/";?>assets/css/style.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.4/css/jquery.dataTables.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css" rel="stylesheet" />
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
+   
+   
+     
+  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" /> 
+   
+   
+ 
+
+</head>
+ 
+
+  
+  
+  
---
+++ application/views/privacypolicy.php
@@ -0,0 +1,159 @@
+   <main id="main">
+    <section class="h-100 mt-5 cart">
+  <div class="container h-100 py-5">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      
+            
+
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content" style="font-size:13px;">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel"><b>Privacy Policy</b></h5>
+                   
+                </div>
+				<div class="modal-body">We value the trust you place in us. we insist upon the highest standards for secure transactions and customer information privacy. </div>
+                <div class="modal-body"><h6><b>Please read the following statement about our information gathering and dissemination practices.</b></h6>
+				<h6 style="font-size:13px;">Note:Our privacy policy is subject to change at any time without notice. To make sure you are aware of any changes, please review this policy periodically.</h6>By visiting this Website Aucjunctionlite you agree to be bound by the terms and conditions of this Privacy Policy. If you do not agree please do not use or access our Website.<br/>
+				 This Privacy Policy is incorporated into and subject to the Terms of Use.</div>
+				 
+				 <div class="modal-body">
+				 <h6>A. Collection of Personally Identifiable Information and other Information</h6>When you use our Website Aucjunctionlite, we collect and store your personal/official information which is provided by you from time to time. Our primary goal in doing so is to provide you a safe, efficient, smooth and customized experience. This allows us to provide services and features that most likely meet your needs, and to customize our Website to make your experience safer and easier. More importantly, while doing so we collect personal information from you that we consider necessary for achieving this purpose.<br/><br/>
+				 This information is compiled and analyzed on an aggregated basis. This information may include the URL that you just came from (whether this URL is on our Website or not), which URL you next go to (whether this URL is on our Website or not), your computer browser information, and your IP address.<br/><br/>
+				We use data collection devices such as "cookies" on certain pages of the Website to help analyze our web page flow, measure promotional effectiveness, and promote trust and safety. "Cookies" are small files placed on your hard drive that assist us in providing our services. We offer certain features that are only available through the use of a "cookie". We also use cookies to allow you to enter your password less frequently during a session. Cookies can also help us provide information that is targeted to your interests. Most cookies are "session cookies," meaning that they are automatically deleted from your hard drive at the end of a session. You are always free to decline our cookies if your browser permits, although in that case you may not be able to use certain features on the Website and you may be required to re-enter your password more frequently during a session.<br/><br/>
+				 Additionally, you may encounter "cookies" or other similar devices on certain pages of the Website that are placed by third parties. We do not control the use of cookies by third parties.<br/><br/>
+				 • If you choose to buy on the Website, we collect information about your buying behaviour.<br/>
+				 • If you transact with us, we collect some additional information, such as a billing address, shipping address, GST Details, a credit / debit card number and a credit / debit card expiration date and/ or other payment instrument details and tracking information from Rtgs, Neft  cheques or money orders.<br/>
+				 • If you choose to post messages on our message boards, chat rooms or other message areas or leave feedback.We will collect that information you provide to us. We retain this information as necessary to resolve disputes, provide customer support or seller/buyer support and troubleshoot problems as permitted by law.<br/>
+				 • If you send us personal correspondence, such as emails or letters, or if other users or third parties send us correspondence about your activities or postings on the Website, we may collect such information into a file specific to you.
+				 </div>
+				 
+				 <div class="modal-body">
+				<h6>B.Use of Demographic / Profile Data / Your Information</h6>
+				We use personal information to provide the services you request. To the extent we use your personal information to market to you, we will provide you the ability to opt-out of such uses. We use your personal information to resolve disputes; troubleshoot problems; help promote a safe service; collect money; measure consumer interest in our products and services, inform you about online and offline offers, products, services, and updates; customize your experience; detect and protect us against error, fraud and other criminal activity; enforce our terms and conditions; and as otherwise described to you at the time of collection.<br/><br/>
+				 With your consent, we will have access to your SMS, contacts in your directory, call history, location and device information and we may request you to provide your PAN and Aadhaar details to check your eligibility for certain products/services including but not limited to providing credit) being offered by us, sellers, our affiliates or our lending partners. We may share this data with our affiliates or our lending partners for the same purposes as mentioned above, however, we will not store any Aadhaar data ourselves. In the event that consent to this such use of data is withdrawn in the future, we will stop collection of such data but continue to store the data (save for Aadhaar data) and use it for internal purposes to further improve our services.<br/><br/>
+				 In our efforts to continually improve our product and service offerings, we collect and analyze demographic and profile data about our users' activity on our Website Aucjunctionlite.<br/>
+				 We identify and use your IP address to help diagnose problems with our server, and to administer our Website. Your IP address is also used to help identify you and to gather broad demographic information.<br/><br/>
+				 We will occasionally ask you to complete optional online surveys. These surveys may ask you for contact information and demographic information (like zip code, age, or income level). We use this data to tailor your experience at our Website, providing you with content that we think you might be interested in and to display content according to your preferences.</div>
+				 
+				 <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Cookies</h5>
+                </div>
+				
+				<div class="modal-body">
+				 A. "cookie" is a small piece of information stored by a web server on a web browser so it can be later read back from that browser. Cookies are useful for enabling the browser to remember information specific to a given user. We place both permanent and temporary cookies in your computer's hard drive. The cookies do not contain any of your personally identifiable information.</div>
+				
+				<div class="modal-body">
+				B. Sharing of personal information
+				We may share personal information with our other corporate entities and affiliates. These entities and affiliates may market to you as a result of such sharing unless you explicitly opt-out.<br/>
+				We may disclose personal information to third parties. This disclosure may be required for us to provide you access to our Services, to comply with our legal obligations, to enforce our User Agreement, to facilitate our marketing and advertising activities, or to prevent, detect, mitigate, and investigate fraudulent or illegal activities related to our Services. We do not disclose your personal information to third parties for their marketing and advertising purposes without your explicit consent.<br/>
+				We may disclose personal information if required to do so by law or in the good faith belief that such disclosure is reasonably necessary to respond to subpoenas, court orders, or other legal process. We may disclose personal information to law enforcement offices, third party rights owners, or others in the good faith belief that such disclosure is reasonably necessary to: enforce our Terms or Privacy Policy; respond to claims that an advertisement, posting or other content violates the rights of a third party; or protect the rights, property or personal safety of our users or the general public.<br/>
+				We and our affiliates will share / sell some or all of your personal information with another business entity should we (or our assets) plan to merge with, or be acquired by that business entity, or re-organization, amalgamation, restructuring of business. Should such a transaction occur that other business entity (or the new combined entity) will be required to follow this privacy policy with respect to your personal information.
+				</div>
+
+				<div class="modal-body">
+				C. Links to Other Sites<br/>
+				Our Website links to other websites that may collect personally identifiable information about you. Aucjunctionlite is not responsible for the privacy practices or the content of those linked websites.
+				</div>
+
+				<div class="modal-body">
+				D. Security Precautions<br/>
+				Our Website has stringent security measures in place to protect the loss, misuse, and alteration of the information under our control. Whenever you change or access your account information, we offer the use of a secure third party server. Once your information is in our possession we adhere to strict security guidelines, protecting it against unauthorized access.
+				</div>
+
+				<div class="modal-body">
+				E. Choice/Opt-Out<br/>
+				We provide all users with the opportunity to opt-out of receiving non-essential (promotional, marketing-related) communications from us on behalf of our partners, and from us in general, after setting up an account.<br/>If you want to remove your contact information from all Aucjunctionlite lists and newsletters, please visit unsubscribe/ mail us at admin@Aucjunctionlite.com
+				</div>
+
+				<div class="modal-body">
+				F. Advertisements on Aucjunctionlite<br/>
+				We use third-party advertising companies to serve ads when you visit our Website. These companies may use information (not including your name, address, email address, or telephone number) about your visits to this and other websites in order to provide advertisements about goods, raw materials and services of interest to you.
+				</div>
+
+				<div class="modal-body">
+				G. Your Consents<br/>
+					By using the Website and/ or by providing your information, you consent to the collection and use of the information you disclose on the Website in accordance with this Privacy Policy, including but not limited to Your consent for sharing your information as per this privacy policy.<br/>If we decide to change our privacy policy, we will post those changes on this page so that you are always aware of what information we collect, how we use it, and under what circumstances we disclose it.
+				</div>
+
+				<div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Officer For Grievance</h5>
+                </div>
+				
+				<div class="modal-body">
+				In accordance with Information Technology Act 2000 and rules made there under, the name and contact details of the Grievance Officer are provided below:<br/>
+					Aucjunctionlite<br/>
+					Shopemet Networks Pvt Ltd.<br/><br/>
+					Corp Off – Plo No 23, 3rd Floor, Hari Prem Complex
+					Binnamangala, Indiranagar 1st Stage, Bangalore<br/>
+					Karnataka, India, 560 038.<br/>
+					Ph No:080-42332722 / +91-9945454505 <br/>
+					Email: admin@Aucjunctionlite.com / admin@shopemet.com<br/>
+					Time: Mon - Sat (10:00 - 18:00)
+				</div>
+              
+            </div>
+        </div>
+    </div>
+	
+
+</div>
+</div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</section>
+    <!-- ======= Breadcrumbs ======= -->
+
+
+  </main><!-- End #main -->
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+ 
